1331899756d9f7b048de261ab39afb6277f6b1c0
sd-buildbot <dev-null@screwdriver.cd>
2019-11-15 18:02:15 +0000

    Update documentation

--- docs/usage.md
+++
@@ -1,6 +0,0 @@
-## Usage overview
-
-The screwdrivercd package provides a number of CI/CD helper scripts, since these scripts are intended
-to be used in Screwdriver CI/CD pipelines they take all arguments from environment variables and not
-command line arguments.
-
--- src/screwdrivercd/documentation/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2019, Oath Inc.
-# Licensed under the terms of the Apache 2.0 license.  See the LICENSE file in the project root for terms
-"""
-screwdrivercd.documentation module
-"""
-from typing import List
-
-
-__all__: List[str] = ['cli', 'exceptions', 'plugin', 'utility', 'mkdocs', 'sphinx']
