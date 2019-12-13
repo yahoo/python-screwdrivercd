1396d390bd38efec8e9c6448711c36e797673ed8
Dwight Hubbard <254983+dwighthubbard@users.noreply.github.com>
2019-12-13 15:22:18 -0800

    Standard directories fix (#73)
    * Use a git log command that works on git 1.8.x and 2.x
    
    * Use a git log command that works on git 1.8.x and 2.x
    
    * Use a git log command that works on git 1.8.x and 2.x
    
    * Use a git log command that works on git 1.8.x and 2.x
    
    * Use a git log command that works on git 1.8.x and 2.x
    
    * Add type check fixes.
    
    * Add type check fixes.
    
    * build fix
    
    * build fix
    
    * build fix
    
    * Add release tagging
    
    * Add release tagging
    
    Minor updates
    
    * Add release tagging
    
    * Add release tagging
    
    * Bandit checks
    
    * Git tagging fix
    
    * Git tagging fix
    
    * Git tagging fix

--- src/screwdrivercd/utility/environment.py
+++ src/screwdrivercd/utility/environment.py
@@ -125,7 +125,7 @@ def standard_directories(command: str='') -> Dict[str, str]:
     if command:
         sep = os.sep
 
-    artifacts_dir: str = os.environ.get('SD_ARTIFACTS_DIR', '')
+    artifacts_dir: str = os.environ.get('SD_ARTIFACTS_DIR', 'artifacts')
     directories: Dict[str, str] = {
         'artifacts': artifacts_dir,
         'documentation': os.path.join(artifacts_dir, f'documentation'),
@@ -134,6 +134,7 @@ def standard_directories(command: str='') -> Dict[str, str]:
         'reports': os.path.join(artifacts_dir, f'reports{sep}{command}'),
     }
     for directory in directories.values():
-        os.makedirs(directory, exist_ok=True)
+        if directory:
+            os.makedirs(directory, exist_ok=True)
 
     return directories
