8b57a57058f84d669de1f3ee96c8aad610195a85
Dwight Hubbard <254983+dwighthubbard@users.noreply.github.com>
2022-07-13 13:07:51 -0700

    Update test_validators.py

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -140,8 +140,13 @@ invalid_style_config = copy.deepcopy(working_config)
 invalid_style_config['src/mypyvalidator/__init__.py'] = b"""a: int =1\n"""
 
 class DepValidatorTestcase(ScrewdriverTestCase):
-    validator_name = 'type_validation'
+    validator_name = 'dependency_validation'
 
+    def setUp(self):
+		super().setUp()
+		import pkg_resources
+		print('safety version =', pkg_resources.get_distribution("safety").version)
+		
     def test__secure_deps(self):
         self.write_config_files(working_config)
         result = validate_with_safety()
